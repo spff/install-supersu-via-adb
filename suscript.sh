8308ab516b9af03bb1279b9bffac0671fba4c4fa
Chin-Wei Chang <spff@bitmessage.ch>
2018-05-11 09:51:05 +0800

    Update suscript.sh

--- suscript.sh
+++ suscript.sh
@@ -1,10 +1,6 @@
-# install-supersu-via-adb
-
 #IMPORTANT!!!
 #THIS SCRIPT IS FOR API21+ arm64 devices. If you want to do for device with other API or architecture,
 #you may read this article [How to install SuperSu on Android via adb](http://selvaline.blogspot.tw/2015/09/how-to-install-supersu-on-android-via.html)
-
-#PLEASE NOTE: Rooting a device may void the warranty on the device. It may also make the device unstable or if not done properly, may completely brick the device. Some methods may install additional apps/software on your device. I don't take any responsibility for your device. Root at your own risk and only if you understand what you are doing!
 
 adb root
 adb remount
